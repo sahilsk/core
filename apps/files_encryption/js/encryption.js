46a7a2a9acb1a2d25420be346ddca9271fc1ad71
Bjoern Schiessle <schiessle@owncloud.com>
2014-02-11 12:44:06 +0100

    don't block login forever if we are stuck in the middle of the initial encryption

---
+++ apps/files_encryption/js/encryption.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2013
+ *  Bjoern Schiessle <schiessle@owncloud.com>
+ * This file is licensed under the Affero General Public License version 3 or later.
+ * See the COPYING-README file.
+ */
+
+OC.Encryption={
+	MIGRATION_OPEN:0,
+	MIGRATION_COMPLETED:1,
+	MIGRATION_IN_PROGRESS:-1,
+};
+
